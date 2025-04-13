fb747a790326cc4415d82b6d78a725f12aa598bb
Sakshi Chandra <154490080+Fitrat25@users.noreply.github.com>
2025-04-13 20:08:37 +0530

    Create Task4.jsx

---
+++ Week3_Assignment/Task4.jsx
@@ -0,0 +1,16 @@
+import React from 'react';
+
+export default function StyledButton() {
+  const handleClick = () => {
+    console.log("Button clicked!");
+  };
+
+  return (
+    <button
+      onClick={handleClick}
+      className="bg-blue-500 text-white px-6 py-2 rounded hover:bg-blue-700 transition"
+    >
+      Click Me
+    </button>
+  );
+}
